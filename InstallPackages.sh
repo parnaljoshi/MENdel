90d7489abc7d826059e26b644335b2effb17e27a
Parnal Joshi <44910482+parnaljoshi@users.noreply.github.com>
2020-09-27 03:43:25 -0500

    Add files via upload

---
+++ InstallPackages.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+echo "Starting to install required Python packages"
+pip install argparse
+pip install pandas
+pip install Bio
+pip install numpy
+pip install scipy
+pip install subprocess
---
+++ Windows_InstallPackages.bat
@@ -0,0 +1,7 @@
+echo "Starting to install required Python packages"
+python -m pip install argparse
+python -m pip install pandas
+python -m pip install Bio
+python -m pip install numpy
+python -m pip install scipy
+python -m pip install subprocess
